f032150409c0135ea13470c6d255e2131f9651c8
kavishghime <kavishghime90@gmail.com>
2023-11-28 17:16:56 -0800

    updated navbar for om sign

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -102,7 +102,7 @@ function Navbar() {
                         </Typography> */}
 
                         <Typography variant="h6">
-                            ॐ
+                            {/* ॐ */}
                         </Typography>
 
                         <IconButton>
